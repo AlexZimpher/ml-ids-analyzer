8d80b5c07a5e09c49ff9916c51ffd724e497dbb4
Alex <61612663+AlexZimpher@users.noreply.github.com>
2025-04-25 19:05:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,15 @@ setup(
         'numpy', 'pandas', 'scikit-learn', 'xgboost',
         'matplotlib', 'seaborn', 'joblib', 'PyYAML', 'shap'
     ],
-    entry_points={
-        'console_scripts': [
-            'mlids-preprocess=preprocess:main',
-            'mlids-train=model:train_model',
-            'mlids-predict=predict:predict_new_data',
-        ]
-    },
-)
+     entry_points={
+     'console_scripts': [
+         'mlids-preprocess=preprocess:main',
+         'mlids-train=model:train_model',
+         'mlids-predict=predict:predict_new_data',
+     ]
+     'console_scripts': [
+         'mlids-preprocess=src.preprocess:main',
+         'mlids-train=src.model:train_model',
+         'mlids-predict=src.predict:predict_new_data',
+     ]
+ }
