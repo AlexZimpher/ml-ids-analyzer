6fb6dcdec1dcebeb8bf3b631b278dca81d32c200
Alex <61612663+AlexZimpher@users.noreply.github.com>
2025-04-25 17:58:03 -0700

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ml_ids_analyzer',
+    version='1.0.0',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'numpy', 'pandas', 'scikit-learn', 'xgboost',
+        'matplotlib', 'seaborn', 'joblib', 'PyYAML', 'shap'
+    ],
+    entry_points={
+        'console_scripts': [
+            'mlids-preprocess=preprocess:main',
+            'mlids-train=model:train_model',
+            'mlids-predict=predict:predict_new_data',
+        ]
+    },
+)
